4ffca1e090774b67eee7de0a421db3465644e6db
MarkoUzelac <145296039+MarkoUzelac@users.noreply.github.com>
2026-09-07 20:32:14 +0200

    Remove Firebase bundle chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
+  plugins: [react()],
+  server: { host: '0.0.0.0', port: 3000 },
   build: {
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    terserOptions: { compress: { drop_console: true, drop_debugger: true } },
     rollupOptions: {
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules/react')) return 'vendor-react';
           if (id.includes('node_modules/lucide-react')) return 'vendor-icons';
-          if (id.includes('node_modules/firebase')) return 'vendor-firebase';
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
